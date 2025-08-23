1022724a1df0b117d4bc44446ed32635fd774bc2
mosco <sajuyigbemoses@gmail.com>
2025-08-23 16:28:33 +0100

    rapha

--- components/IfaHome.jsx
+++ components/IfaHome.jsx
@@ -16,7 +16,7 @@ const josefinSans = Josefin_Sans({
 const IfaHome = () => {
   return (
     <Container maxWidth={false}>
-      <div className="grid md:grid-cols-2 lg:mx-15 mx-2 mb-150 mt-20 grid-cols-1 gap-5">
+      <div className="grid md:grid-cols-2 lg:mx-15 mx-0 mb-150 mt-20 grid-cols-1 md:gap-5">
         {/* 1st col */}
 
         <motion.div
@@ -25,9 +25,9 @@ const IfaHome = () => {
       transition={{ duration: 0.5, delay: 0.2, ease: 'easeInOut' }}
         
         className="relative learning-ifa ">
-          <div className="border-[10px] absolute  border-[#74B117] lg:h-[89vh] h-[57vh] lg:w-[80vh] md:w-[50vh] w-[35vh] pb-10 md:pb-0 "></div>
+          <div className="border-[10px] absolute  border-[#74B117] lg:h-[89vh] h-[44vh] lg:w-[80vh] w-[40vh] "></div>
 
-          <div className="border-2 bg-white absolute lg:left-[-30px] left-[-10px] top-2 border-[#74B117] lg:w-[80vh] w-[48vh] lg:p-8 p-3 lg:h-[80vh] h-[47vh] mt-5 ">
+          <div className="border-2 bg-white absolute lg:left-[-30px] left-[-10px] top-2 border-[#74B117] lg:w-[80vh] w-[38vh] lg:p-8 p-3 lg:h-[80vh] h-[42vh] mt-5 ">
             <p className="capitalize lg:text-[50px] md:text-[40px] text-[25px] font-semibold leading-tight">
               Join thousands{" "}
               <span className="italic text-[#74B117]">learning Ifá</span>{" "}
